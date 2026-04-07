1e97549b7b66392ccf4ded813d6871d859310e6a
noyseg <n3segal@gmail.com>
2026-04-07 16:26:20 +0000

    Add memsize system call
    - Added SYS_memsize (22) to syscall.h
    - Added extern declaration and syscalls array mapping in syscall.c
    - Implemented sys_memsize in sysproc.c returning process memory size (p->sz)
    - Added memsize() function prototype to user.h
    - Added memsize entry to usys.pl for userspace system call stub
    - Created memsize_test.c userspace test program that:
      * Prints initial process memory size
      * Allocates 20KB with malloc
      * Prints memory size after allocation
      * Frees the memory
      * Prints memory size after freeing
    - Updated Makefile to include memsize_test in UPROGS

---
+++ user/memsize_test.c
@@ -0,0 +1,33 @@
+#include "kernel/types.h"
+#include "user/user.h"
+
+int
+main(void)
+{
+  int initial_size, after_alloc, after_free, after_free1;
+  char *p;
+
+  // Print initial memory size
+  initial_size = memsize();
+  printf("Initial memory size: %d bytes\n", initial_size);
+
+  // Allocate 20k more bytes
+  p = malloc(20480);
+  if(p == 0) {
+    printf("malloc failed\n");
+    exit(1);
+  }
+
+  // Print memory size after allocation
+  after_alloc = memsize();
+  printf("Memory size after allocating 20k: %d bytes\n", after_alloc);
+
+  // Free the allocated array
+  free(p);
+
+  // Print memory size after release
+  after_free = memsize();
+  printf("Memory size after freeing: %d bytes\n", after_free);
+  
+  exit(0);
+}
